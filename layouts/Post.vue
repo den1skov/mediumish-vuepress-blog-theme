959d5ef8265af4409f1c2793308f30ce1c759788
denis Kovalchuk <den.kovalchuk@hotmail.com>
2021-12-22 00:26:01 -0800

    SK - correct course ID

--- layouts/Post.vue
+++ layouts/Post.vue
@@ -34,7 +34,7 @@ itemtype="https://schema.org/BlogPosting"
 <div id="firewall-rules"
                     class="sk-widget"
                     data-width="600"
-                    data-course-id="3"
+                    data-course-id="1"
                     data-client-id="86bfa9f2-6238-4241-83be-fd961e72b996">
                 </div>
 
