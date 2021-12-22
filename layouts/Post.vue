11a92a70328b66b3bc1630495c813d4d35b9af19
denis Kovalchuk <den.kovalchuk@hotmail.com>
2021-12-21 22:57:22 -0800

    SK js - 2nd attempt

--- layouts/Post.vue
+++ layouts/Post.vue
@@ -39,6 +39,13 @@ itemtype="https://schema.org/BlogPosting"
 
   </div>
 </div>
+<div id="firewall-rules"
+                style="display: none" 
+                class="sk-widget"
+                data-width="600"
+                data-course-id="1"
+                data-client-id="86bfa9f2-6238-4241-83be-fd961e72b996">
+            </div>
 
 </article>
 
@@ -71,8 +78,15 @@ export default {
 }
 </script>
 
-<script async defer data-id="sk-runtime" src="https://microlearn.azurewebsites.net/js/bundle/runtime.js"></script>
+<script data-id="sk-runtime" src="https://microlearn.azurewebsites.net/js/bundle/runtime.js"></script>
 
 <style lang="stylus" scoped>
 @import url('https://fonts.googleapis.com/css?family=Merriweather:400,400i,700&display=swap')
 </style>
+
+<style type="text/css">
+      .sk-widget .sk-text {
+          font-size: 1.5rem;
+          font-weight: 400;
+      }
+  </style>
